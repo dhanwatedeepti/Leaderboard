60014761eb4b2f8f9854d1e8bd1c62c420b50c75
Deepti Dhanwate <dhanwatedeepti2@gmail.com>
2025-06-27 10:36:23 +0530

    Initial push of leaderboard app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+import pandas as pd
+
+app = Flask(__name__, template_folder='templates')  # Make sure HTML is in /templates
+CORS(app)
+
+EXCEL_FILE = 'Leaderboard.xlsx'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/Leaderboard')
+def leaderboard():
+    df = pd.read_excel(EXCEL_FILE, sheet_name="users")
+    df = df[['Name', 'Score', 'Email']].sort_values(by='Score', ascending=False)
+    players = df.to_dict(orient='records')
+    return jsonify([
+        {"name": p['Name'], "score": int(p['Score']), "email": p['Email']} for p in players
+    ])
+
+if __name__ == '__main__':
+    app.run(debug=True)
